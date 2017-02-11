972ac411dbf8d6205502b909adfb3020bf49c7f7
michael yee <michael.yee@triblio.com>
2017-02-11 02:01:11 -0500

    init zip

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+//= require jquery.js
+//= require bootstrap.min.js
+//= require plugins/morris/raphael.min.js
+//= require plugins/morris/morris.min.js
+//= require plugins/morris/morris-data.js
---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(require("connect-assets")());
+app.set('view engine','pug');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.listen(9000, function () {
+  console.log('Example app listening on port 9000!')
+})
